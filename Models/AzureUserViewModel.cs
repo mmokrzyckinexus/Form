9772ebd4787a4d4ce2b4e921063b4d275c14a014
mmokrzyckinexus <mmokrzycki@nexuspharma.net>
2026-03-23 14:31:01 -0500

    changes

---
+++ Models/AzureUserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Graph.Models;
+
+namespace Form.Models
+{
+    public class AzureUserViewModel
+    {
+        public User User { get; set; }
+        public DirectoryObject Manager { get; set; }
+        public IList<User> DirectReports { get; set; }
+        public IList<DirectoryObject> MembersOf { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+//using System.IdentityModel.Tokens.Jwt;
+//using Microsoft.Graph;
+using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
+/*using Microsoft.Net.Http.Headers;
+using Microsoft.Kiota.Abstractions;
+using Microsoft.Graph.Authentication;
+using Microsoft.Kiota.Abstractions.Authentication;*/
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Sign-in users with the Microsoft identity platform
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAD")).EnableTokenAcquisitionToCallDownstreamApi().AddMicrosoftGraph(builder.Configuration.GetSection("Graph")).AddInMemoryTokenCaches();
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
